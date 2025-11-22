c3cd2a9ce58ca2a2f2ec93675e787008aa8c2a08
luchenya <1934600272@qq.com>
2025-11-22 11:10:55 +0800

    学习ws2812跑马灯效果, 将三个颜色依次跑马灯显示。

--- ledc/main/main.c
+++ ledc/main/main.c
@@ -11,12 +11,12 @@
 #include "esp32/rom/ets_sys.h"
 #include "driver/ledc.h"
 
-/* 定义LED的GPIO口 */
+/* 定义 LED 的 GPIO 口 */
 #define LED_GPIO  GPIO_NUM_27
 
 #define TAG     "LEDC"
 
-/* LED闪烁运行任务 */
+/* LED 闪烁运行任务 */
 void led_run_task(void* param)
 {
     int iGPIOLevel = 0;
@@ -28,7 +28,7 @@ void led_run_task(void* param)
     }
 }
 
-/* LED闪烁初始化 */
+/* LED 闪烁初始化 */
 void led_flash_init(void)
 {
     gpio_config_t led_gpio_cfg = {
@@ -49,61 +49,75 @@ void led_flash_init(void)
 #define LEDC_OUTPUT_IO          (LED_GPIO)              //选择GPIO端口
 #define LEDC_CHANNEL            LEDC_CHANNEL_0          //PWM通道
 #define LEDC_DUTY_RES           LEDC_TIMER_13_BIT       //分辨率
-#define LEDC_DUTY               (4095)                  //渐变到目标占空比值，这里设置为2^13-1
+#define LEDC_DUTY               (4095)                  //渐变到目标占空比值，这里设置为2^12-1,最大可设置为2^13-1
 #define LEDC_FREQUENCY          (5000)                  //PWM周期
 
 //用于通知渐变完成
 static EventGroupHandle_t   s_ledc_ev = NULL;
 
-//关灯完成事件标志
-#define LEDC_OFF_EV  (1<<0)
+//关灯完成事件标志( 事件组第0位 )
+#define LEDC_OFF_EV  (1UL<<0)
 
-//开灯完成事件标志
-#define LEDC_ON_EV   (1<<1)
+//开灯完成事件标志( 事件组第1位 )
+#define LEDC_ON_EV   (1UL<<1)
 
-//渐变完成回调函数
-bool IRAM_ATTR ledc_finish_cb(const ledc_cb_param_t *param, void *user_arg)
+/* 渐变完成回调函数 
+ * IRAM_ATTR 放在中断服务函数前面，表示该函数放在内部RAM中执行，提升执行效率
+ */
+bool IRAM_ATTR ledc_finish_cb(const ledc_cb_param_t *param, void *user_arg) 
 {
     BaseType_t xHigherPriorityTaskWoken;
-    if(param->duty)
-    {
+    if(param->duty){
+        /* 完成占空比从 0 到 LEDC_DUTY 的渐变*/
         xEventGroupSetBitsFromISR(s_ledc_ev,LEDC_ON_EV,&xHigherPriorityTaskWoken);
-    }
-    else
-    {
+    }else{
+        /* 完成占空比从 LEDC_DUTY 到 0 的渐变*/
         xEventGroupSetBitsFromISR(s_ledc_ev,LEDC_OFF_EV,&xHigherPriorityTaskWoken);
     }
+
+    if(xHigherPriorityTaskWoken) {
+        portYIELD_FROM_ISR();
+    }
+    /* 
+     * 这个返回值是一个布尔值，表示是否有更高优先级的任务被唤醒。
+     * 如果返回值为pdTRUE，则意味着在ISR中唤醒了一个更高优先级的任务，
+     * 因此可能需要执行一次上下文切换（通常通过portYIELD_FROM_ISR())
+    */
     return xHigherPriorityTaskWoken;
 }
 
-//ledc 渐变任务
+/* ledc 渐变任务 */
 void ledc_breath_task(void* param)
 {
+    /* 该变量不需要初始化，因为它总能被 xEventGroupWaitBits 函数设置一个确定的值*/
     EventBits_t ev;
-    while(1)
-    {
+    while(1){
+        /* 
+         * 如果成功等到事件，返回事件组中被设置的位掩码
+         * 如果超时未等到事件, 返回事件组当前的值（0）
+         * FreeRTOS保证函数总是返回一个确定的事件位掩码
+         */
         ev = xEventGroupWaitBits(s_ledc_ev,LEDC_ON_EV|LEDC_OFF_EV,pdTRUE,pdFALSE,pdMS_TO_TICKS(5000));
-        if(ev)
-        {
-            //设置LEDC开灯渐变
-            if(ev & LEDC_OFF_EV)
-            {
+        if(ev){
+            if(ev & LEDC_OFF_EV){      // 检测到灯关闭，设置LEDC开灯渐变
                 ledc_set_fade_with_time(LEDC_MODE,LEDC_CHANNEL,LEDC_DUTY,2000);
                 ledc_fade_start(LEDC_MODE,LEDC_CHANNEL,LEDC_FADE_NO_WAIT);
-            }
-            else if(ev & LEDC_ON_EV)    //设置LEDC关灯渐变
-            {
+            }else if(ev & LEDC_ON_EV){ // 检测到灯打开，设置LEDC关灯渐变
                 ledc_set_fade_with_time(LEDC_MODE,LEDC_CHANNEL,0,2000);
                 ledc_fade_start(LEDC_MODE,LEDC_CHANNEL,LEDC_FADE_NO_WAIT);
             }
-            //再次设置回调函数
+            /* 再次设置回调函数
+             * 每个回调函数在注册后只会执行一次
+             * 当渐变完成触发回调后，系统会自动注销该回调函数
+             * 如果要再次接收渐变完成事件，必须重新注册回调函数
+            */
             ledc_cbs_t cbs = {.fade_cb=ledc_finish_cb,};
             ledc_cb_register(LEDC_MODE,LEDC_CHANNEL,&cbs,NULL);
         }
     }
 }
 
-/* LED呼吸灯初始化 */
+/* LED 呼吸灯初始化(借助 ledc 实现, 无需对 GPIO 引脚初始化) */
 void led_breath_init(void)
 {
     /* 1、初始化一个定时器 */
@@ -136,16 +150,16 @@ void led_breath_init(void)
     ledc_fade_func_install(0);
 
     /* 4、(可选)配置 ledc 渐变 */
-    ledc_set_fade_with_time(LEDC_MODE,LEDC_CHANNEL,LEDC_DUTY,2000);
+    ledc_set_fade_with_time(LEDC_MODE,LEDC_CHANNEL,LEDC_DUTY,2000);// 占空比从0渐变到 LEDC_DUTY，时间2000ms
 
-    //启动渐变
+    /* 5、启动渐变 */
     ledc_fade_start(LEDC_MODE,LEDC_CHANNEL,LEDC_FADE_NO_WAIT);
 
-    //设置渐变完成回调函数
+    /* 6、设置渐变完成回调函数 */
     ledc_cbs_t cbs = {.fade_cb=ledc_finish_cb,};
     ledc_cb_register(LEDC_MODE,LEDC_CHANNEL,&cbs,NULL);
 
-    //创建一个事件组，用于通知任务渐变完成
+    /* 7、创建一个事件组，用于通知任务渐变完成 */
     s_ledc_ev = xEventGroupCreate();
 
     xTaskCreatePinnedToCore(ledc_breath_task,"ledc",2048,NULL,3,NULL,1);
