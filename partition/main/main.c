a4436fc839916e863209c5cd679260298af2597c
luchenya <1934600272@qq.com>
2025-12-21 08:51:03 +0800

    学习使用自定义分区表, 划分自定义分区, 对其进行读写 idf.py menuconfig: 配置 partitions table 为用户自定义分区表文件

--- partition/main/main.c
+++ partition/main/main.c
@@ -5,37 +5,35 @@
 #include "esp_partition.h"
 #include <string.h>
 
-static const char* TAG = "main";
+static const char *TAG = "main";
 
-#define USER_PARTITION_TYPE     0x40        //自定义的分区类型
-#define USER_PARTITION_SUBTYPE  0x01        //自定义的分区子类型
+#define USER_PARTITION_TYPE 0x40    // 自定义的分区类型
+#define USER_PARTITION_SUBTYPE 0x01 // 自定义的分区子类型
 
-//分区指针
-static const esp_partition_t* partition_res=NULL;
+/* 分区指针 */ 
+static const esp_partition_t *pxPartitionRes = NULL; // 承载用户自定义分区的首地址
 
-//读取缓存
-static char g_esp_buf[1024];
+/* 读取缓存 */ 
+static char cEspBuffer[1024];
 
 void app_main(void)
 {
-    //找到自定义分区，返回分区指针，后续用到这个指针进行各种操作
-    partition_res=esp_partition_find_first(USER_PARTITION_TYPE,USER_PARTITION_SUBTYPE,"user");
-    if(partition_res == NULL)
-    {
-        ESP_LOGI(TAG,"Can't find partition,return");
+    /* 找到符合要求的第一个分区，返回分区指针，后续用到这个指针进行各种操作 */ 
+    pxPartitionRes = esp_partition_find_first(USER_PARTITION_TYPE, USER_PARTITION_SUBTYPE, "user");
+    if (pxPartitionRes == NULL){
+        ESP_LOGI(TAG, "Can't find partition,return");
         return;
     }
-    //擦除
-    ESP_ERROR_CHECK(esp_partition_erase_range(partition_res,0x0,0x1000));
-    //测试字符串
-    const char* test_str = "this is for test string";
-    //从分区偏移位置0x0写入字符串
-    ESP_ERROR_CHECK(esp_partition_write(partition_res,0x00, test_str, strlen(test_str)));
-    //从分区偏移位置0x0读取字符串
-    ESP_ERROR_CHECK(esp_partition_read(partition_res,0x00, g_esp_buf, strlen(test_str)));
-    ESP_LOGI(TAG,"Read partition str:%s",g_esp_buf);
-    while(1)
-    {
+    /* 擦除 */ 
+    ESP_ERROR_CHECK(esp_partition_erase_range(pxPartitionRes, 0x0, 0x1000));
+    /* 测试字符串 */ 
+    const char *test_str = "this is for test string";
+    /* 从分区偏移位置0x0写入字符串 */ 
+    ESP_ERROR_CHECK(esp_partition_write(pxPartitionRes, 0x00, test_str, strlen(test_str)));
+    /* 从分区偏移位置0x0读取字符串 */ 
+    ESP_ERROR_CHECK(esp_partition_read(pxPartitionRes, 0x00, cEspBuffer, strlen(test_str)));
+    ESP_LOGI(TAG, "Read partition str:%s", cEspBuffer);
+    while (1){
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
